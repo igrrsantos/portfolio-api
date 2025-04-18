d1418141d9c3872c240078342c9aedd584f0e359
Igor Santos <igrrsantos@gmail.com>
2025-04-18 17:11:53 -0300

    teste

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,4 @@
+puts "Porta usada pela aplicação: #{ENV['PORT']}"
 # This configuration file will be evaluated by Puma. The top-level methods that
 # are invoked here are part of Puma's configuration DSL. For more information
 # about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
